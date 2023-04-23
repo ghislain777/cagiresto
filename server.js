8496af7538c078ff267bd7447c838612c338bfae
Ghislain <tghislain@gmail.com>
2023-04-23 14:38:40 +0000

    serverjs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const winston = require('winston');
 const { Role } = require('./models')
 const roleRoutes = require('./routes/role_routes');
 const utilisateurRoutes = require('./routes/utilisateur_routes');
@@ -7,17 +8,32 @@ const bodyParser = require('body-parser');
 const menuRoutes = require('./routes/menu_route');
 const activiteRoutes = require('./routes/activite_routes');
 const privilegeRoutes = require('./routes/privilege_routes');
- const centreRoutes = require('./routes/centre_routes.js')
- const dpeRoutes = require('./routes/dpe_routes.js')
- const ireRoutes = require('./routes/ire_routes.js')
- const profileRoutes = require('./routes/profile_routes.js')
- const examenRoutes = require('./routes/examen_routes.js')
- const profildexamenRoutes = require('./routes/profildexamen_routes.js')
- const parametreRoutes = require('./routes/parametre_routes.js')
- const disciplineRoutes = require('./routes/discipline_routes.js')
- const importationRoutes = require('./routes/importation_routes.js')
- const resultatRoutes = require('./routes/resultat_routes.js')
- const noteresultatRoutes = require('./routes/noteresultat_routes.js')
+const payRoutes = require('./routes/pay_routes.js')
+const villeRoutes = require('./routes/ville_routes.js')
+const communeRoutes = require('./routes/commune_routes.js')
+const quartierRoutes = require('./routes/quartier_routes.js')
+ const categorieRoutes = require('./routes/categorie_routes.js')
+ const repaRoutes = require('./routes/repa_routes.js')
+ const commandeRoutes = require('./routes/commande_routes.js')
+ const lignecommandeRoutes = require('./routes/lignecommande_routes.js')
+ const modedepayementRoutes = require('./routes/modedepayement_routes.js')
+ const adresseRoutes = require('./routes/adresse_routes.js')
+ const clientRoutes = require('./routes/client_routes.js')
+ const accompagnementRoutes = require('./routes/accompagnement_routes.js')
+ const ligneaccompagnementRoutes = require('./routes/ligneaccompagnement_routes.js')
+ const livreurRoutes = require('./routes/livreur_routes.js')
+ const prixlivraisonRoutes = require('./routes/prixlivraison_routes.js')
+
+
+
+
+
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: 'logs.log' })
+  ]
+});
 
 
 
@@ -25,8 +41,8 @@ const PORT = process.env.PORT || 8000
 const app = express()
 const path = require('path');
 
+
 app.use(express.static('public')); // servir les fichiers du repertoire public comme statiques
-app.use(express.static('.well-known')); // servir les fichiers du repertoire public comme statiques
 //app.use(express.static(path.join(__dirname, 'public')));
 // app.use((req, res, next) => {
 //   logger.info(`${req.method} ${req.url}`);
@@ -43,26 +59,22 @@ app.use('/api/utilisateurs', utilisateurRoutes);
 app.use('/api/menus', menuRoutes);
 app.use('/api/activites', activiteRoutes);
 app.use('/api/privileges', privilegeRoutes);
-app.use('/api/centres', centreRoutes); 
-app.use('/api/dpes', dpeRoutes); 
-app.use('/api/ires', ireRoutes); 
-app.use('/api/profiles', profileRoutes); 
-app.use('/api/examens', examenRoutes); 
-app.use('/api/profildexamens', profildexamenRoutes); 
-app.use('/api/parametres', parametreRoutes); 
-app.use('/api/disciplines', disciplineRoutes); 
-app.use('/api/importations', importationRoutes); 
-app.use('/api/resultats', resultatRoutes); 
-app.use('/api/noteresultats', noteresultatRoutes); 
-
-
-
-
-
-
-
-
-
+app.use('/api/pays', payRoutes);
+app.use('/api/villes', villeRoutes);
+app.use('/api/communes', communeRoutes);
+app.use('/api/quartiers', quartierRoutes); 
+app.use('/api/categories', categorieRoutes); 
+app.use('/api/repas', repaRoutes); 
+app.use('/api/commandes', commandeRoutes);
+app.use('/api/commandes', commandeRoutes); 
+app.use('/api/lignecommandes', lignecommandeRoutes); 
+app.use('/api/modedepayements', modedepayementRoutes); 
+app.use('/api/adresses', adresseRoutes); 
+app.use('/api/clients', clientRoutes); 
+app.use('/api/accompagnements', accompagnementRoutes); 
+app.use('/api/ligneaccompagnements', ligneaccompagnementRoutes); 
+app.use('/api/livreurs', livreurRoutes); 
+app.use('/api/prixlivraisons', prixlivraisonRoutes); 
 
 
 
@@ -100,8 +112,9 @@ const multerStorage = multer.diskStorage({
     res.json(`${req.file.filename}`);
   });
 
+
 app.listen(PORT, '0.0.0.0', () => {
-  logger.info(`Express server listening on port ${PORT}`)
+  console.log(`Express server listening on port `, PORT)
 });
 
 // app.get('/', (req, res) => {
